b19a5ae343530a5679eda656d791e793e065e8cc
Tabriz Nuruyev <nuriyevtebriz@gmail.com>
2025-11-23 18:49:08 +0100

    pre-commit test

--- src/langchain_deepagents/main.py
+++ src/langchain_deepagents/main.py
@@ -1,9 +1,9 @@
-import os
 from typing import Literal
 
 from deepagents import create_deep_agent
-from langchain.chat_models import init_chat_model
-from langgraph.types import Command
+
+# from langchain.chat_models import init_chat_model
+# from langgraph.types import Command
 from tavily import TavilyClient
 
 from config import settings
@@ -11,9 +11,7 @@ from config import settings
 tavily_client = TavilyClient(api_key=settings.tavily_api_key)
 
 
-import sys
-
-print(sys.path)
+# print(sys.path)
 
 
 def internet_search(
@@ -32,13 +30,18 @@ def internet_search(
 
 
 # System prompt to steer the agent to be an expert researcher
-research_instructions = """You are an expert researcher. Your job is to conduct thorough research and then write a polished report.
+research_instructions = """
+You are an expert researcher. Your job is to conduct 
+thorough research and then write a polished report.
 
-You have access to an internet search tool as your primary means of gathering information.
+You have access to an internet search tool as your primary means of gathering 
+information.
 
 ## `internet_search`
 
-Use this to run an internet search for a given query. You can specify the max number of results to return, the topic, and whether raw content should be included.
+Use this to run an internet search for a given query. 
+You can specify the max number of results to return, the topic, 
+and whether raw content should be included.
 """
 
 agent = create_deep_agent(tools=[internet_search], system_prompt=research_instructions)
